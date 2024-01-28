d05ce47a3f3d75204dc913a47df07504ce95ad9b
Joselon <jose.navarro.osta@gmail.com>
2024-01-28 16:54:45 +0000

    VersionHTML 0.32 fix pages

--- src/views/html/MainMenu.js
+++ src/views/html/MainMenu.js
@@ -19,31 +19,19 @@ export class MainMenu {
         let tabContainer = document.createElement('section');
         let contentContainer = document.createElement('section');
 
-        let tabsElement = document.createElement('dile-tabs');
-        tabsElement.id = 'tabs';
-        tabsElement.setAttribute(`attrForSelected`, 'name');
-        tabsElement.setAttribute(`selected`, 'categories');
-        tabsElement.setAttribute(`selectorId`, 'tabs');
-
-        let contentElement = document.createElement('dile-pages');
-        contentElement.id = "pages";
-        contentElement.setAttribute(`attrForSelected`, 'name');
-        contentElement.setAttribute(`selected`, 'categories');
-        contentElement.setAttribute(`selectorId`, 'tabs');
+        let tabsHtml = '<dile-tabs attrForSelected="name" selected="categories" selectorId="tabs">';
+        let contentHtml = '<dile-pages attrForSelected="name" selected="categories" selectorId="tabs">';
 
         let optionsHtml = '';
-        let contentHtml = '';
         for (let option of this._options) {
             optionsHtml += `<dile-tab name=${option[0]}>${option[1]}</dile-tab>`;
             contentHtml += `<div name=${option[0]}><p>${option[1]}</p></div>`;
         }
-        tabsElement.innerHTML = optionsHtml;
-        tabContainer.append(tabsElement);
-        contentElement.innerHTML = contentHtml;
-        contentContainer.append(contentElement);
-        this._menuElement.appendChild(tabContainer);
-        this._menuElement.appendChild(contentContainer);
-
+        tabsHtml += optionsHtml+'</dile-tabs>';
+        tabContainer.innerHTML = tabsHtml;
+        contentHtml += '</dile-pages>';
+        contentContainer.innerHTML= contentHtml;
+        this._menuElement.append(tabContainer,contentContainer);
     }
 
     interact() {
