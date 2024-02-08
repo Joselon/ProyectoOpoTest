c993ede78b8a659bb4236e9dd5daeca73b0a04c3
Joselon <jose.navarro.osta@gmail.com>
2024-02-08 11:06:37 +0000

    VersionHTML 1.9.100 dile-radio-group updated

--- src/views/html/components/jno-question-builder.js
+++ src/views/html/components/jno-question-builder.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
-import '../../../utils/view/html/components/jno-dile-radio-group.js';
-import '../../../utils/view/html/components/jno-dile-radio.js'
+import '@dile/dile-radio-group/dile-radio-group.js';
+import '@dile/dile-radio-group/dile-radio.js';
 import { repeat } from 'lit/directives/repeat.js';
 
 
@@ -55,23 +55,36 @@ export class JnoQuestionBuilder extends LitElement {
         <dile-modal id="elmodal" showCloseIcon blocking>
                 <h2>Nueva Pregunta</h2>
                 <form id="elform">
-                <jno-dile-radio-group id="target" name="statement-target" label="Propuestas de Enunciado:" @jno-dile-radio-selected="${this.updateTypes}">
+                <dile-radio-group
+                 id="target"
+                 name="statement-target"
+                 label="Propuestas de Enunciado:"
+                 @dile-radio-selected="${this.updateTypes}"
+                 >
                     ${repeat(this.statementTargets, (group, indexGroup) => html`
                       ${repeat(this.statementTargets[indexGroup], (item, index) => html`
-                    <jno-dile-radio
+                    <dile-radio
                          .label="${this.statementTargetsTitles[indexGroup][index]}"
                          .name="${this.statementTargetsTitles[indexGroup][index]}"
-                         value="${item}"
-                         ></jno-dile-radio>
+                         .value="${item}"
+                         ></dile-radio>
                          
                      `)}
                     `)}
-                </jno-dile-radio-group>
-                <jno-dile-radio-group id="type" name="question-type" label="Tipo de Pregunta Disponibles:" @dile-radio-group-changed="${this.enabledAdd}">
+                </dile-radio-group>
+                <dile-radio-group 
+                    id="type"
+                    name="question-type"
+                    label="Tipo de Pregunta Disponibles:"
+                    @dile-radio-group-changed="${this.enabledAdd}"
+                >
                     ${repeat(this.questionTypes, (item, index) => html`
-                    <jno-dile-radio label="${this.questionTypeTitles[index]}" value="${item}"></jno-dile-radio>
+                    <dile-radio
+                     label="${this.questionTypeTitles[index]}"
+                     value="${item}"
+                     ></dile-radio>
                     `)}
-                </jno-dile-radio-group>
+                </dile-radio-group>
                 <dile-input label="Enunciado" name="statement" id="statement" placeholder="Copie el tipo de enunciado" ></dile-input>
                 </form>
                 <button type="button" @click=${this.insertQuestion} ?disabled=${!this.enabled}>Añadir</button>
@@ -80,6 +93,7 @@ export class JnoQuestionBuilder extends LitElement {
     }
 
     newQuestion(userState) {
+        this.elstatement.value = '';
         this.userState = userState;
         this.category = this.userState.getCurrentCategory();
         this.concept = this.userState.getCurrentConcept();
@@ -88,7 +102,7 @@ export class JnoQuestionBuilder extends LitElement {
         this.statementTargets = [];
         this.statementTargetsTitles = [];
         [this.statementTargets, this.statementTargetsTitles] = this.questionBuilder.getStatementsAvailablesInConcept();
-        this.statementTargets = [...this.statementTargets];
+       // this.statementTargets = [...this.statementTargets];
         this.elmodal.open();
     }
 
@@ -124,5 +138,6 @@ export class JnoQuestionBuilder extends LitElement {
             detail: 'model-changed'
         }));
     }
+
 }
 customElements.define('jno-question-builder', JnoQuestionBuilder);
