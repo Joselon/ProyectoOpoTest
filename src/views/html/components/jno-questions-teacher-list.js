798307c157cf81c34ed6610a60bc7b9f62e8f94b
Joselon <jose.navarro.osta@gmail.com>
2024-02-14 19:20:26 +0000

    Filtro Preguntas del profesor

--- src/views/html/components/jno-questions-teacher-list.js
+++ src/views/html/components/jno-questions-teacher-list.js
@@ -1,15 +1,31 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { UpdateAtModelChangedMixin } from '../mixins/UpdateAtModelChangedMixin.js';
 import { repeat } from 'lit/directives/repeat.js';
+import { filterIcon } from '@dile/icons';
 import './jno-question-teacher.js';
 import './jno-question-builder.js';
 
 export class JnoQuestionsTeacherList extends UpdateAtModelChangedMixin(LitElement) {
 
+    static styles = [
+        css`
+            :host {
+                display: block;
+            }
+            .clicked {
+                --dile-button-background-color: #491449;
+            }
+            #filter {
+                float:right;
+            }
+        `
+    ];
+
     static get properties() {
         return {
             elements: { type: Array },
             userState: { type: Object },
+            isFiltered: { type: Boolean }
         };
     }
 
@@ -17,6 +33,7 @@ export class JnoQuestionsTeacherList extends UpdateAtModelChangedMixin(LitElemen
         super();
         this.elements = [];
         this.userState = {};
+        this.isFiltered = false;
     }
 
     render() {
@@ -31,7 +48,8 @@ export class JnoQuestionsTeacherList extends UpdateAtModelChangedMixin(LitElemen
             return html`<p>Debe seleccionar una categoría</p>`;
         }
         else if (this.userState.getCurrentConcept().getKeyword() === '---') {
-            this.elements = this.userState.getCurrentCategory().getQuestions();
+            if(!this.isFiltered)
+                this.elements = this.userState.getCurrentCategory().getQuestions();
             message = html`<p>Contenidas en <b>${this.userState.getCurrentCategory().getName()}</b> (sin subcategorías)</p>`;
             return html`<p>Debe seleccionar un concepto para añadir preguntas</p>
             ${message}
@@ -39,7 +57,8 @@ export class JnoQuestionsTeacherList extends UpdateAtModelChangedMixin(LitElemen
             `;
         }
         else {
-            this.elements = this.userState.getCurrentCategory().getConceptQuestions(this.userState.getCurrentConceptKey());
+            if(!this.isFiltered)
+                this.elements = this.userState.getCurrentCategory().getConceptQuestions(this.userState.getCurrentConceptKey());
             return html`
             <p>Contenidas en <b>${this.userState.getCurrentCategory().getName()}</b> y relacionadas con el Concepto: <b>${this.userState.getCurrentConcept().getKeyword()}</b></p>
             <dile-button-icon @click=${this.newQuestion}>Añadir</dile-button-icon>
@@ -52,6 +71,7 @@ export class JnoQuestionsTeacherList extends UpdateAtModelChangedMixin(LitElemen
         let actionOptions = ["Desactivar"];
         let extraActionHtml = html`<jno-question-builder></jno-question-builder>`;
         return html`
+            <dile-button-icon id="filter" .icon=${filterIcon} @click="${this.filterQuestions}" class="${this.isFiltered?'clicked':''}">${this.isFiltered?'Eliminar filtro':'Filtrar Pendientes'}</dile-button-icon>
             ${repeat(this.elements, (element) => html`
                     <jno-question-teacher 
                         .element="${element}"
@@ -65,8 +85,31 @@ export class JnoQuestionsTeacherList extends UpdateAtModelChangedMixin(LitElemen
         `;
     }
 
-    newQuestion() {
+    filterQuestions(e) {
+        if (!this.isFiltered) {
+            this.isFiltered = true;
+            let pendingReviewQuestions = [];
+            for (let question of this.elements) {
+                for (let answer of question.getAnswers()) {
+                    if (!answer.isEvaluated() && !pendingReviewQuestions.includes(question)) {
+                        pendingReviewQuestions.push(question);
+                    }
+                }
+            }
+            this.elements = pendingReviewQuestions;
+        }
+        else {
+            this.elements = [...this.elements];
+            this.isFiltered = false;
+        }
+    }
+
+    newQuestion(e) {
         this.shadowRoot.querySelector('jno-question-builder').newQuestion(this.userState);
+    }
+    stateUpdate(e) {
+        this.isFiltered = false;
+        super.stateUpdate(e);
     }
 }
 customElements.define('jno-questions-teacher-list', JnoQuestionsTeacherList);
